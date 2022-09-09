faf08678d65826fb05da40f7cb242e45ed0ab1da
Gabriel Santo <gvgesanto2@gmail.com>
2022-09-09 20:52:07 -0300

    fix: Fix linters errors

--- solver.rb
+++ solver.rb
@@ -1,19 +1,21 @@
 class Solver
-  def factorial(n)
-    return 1 if(n < 2)
-    n * factorial(n - 1)
+  def factorial(num)
+    return 1 if num < 2
+
+    num * factorial(num - 1)
   end
 
   def reverse(word)
-    reversedWord = ''
-    (word.length - 1).downto(0) { |i| reversedWord += word[i] }
-    reversedWord
+    reversed_word = ''
+    (word.length - 1).downto(0) { |i| reversed_word += word[i] }
+    reversed_word
   end
 
-  def fizzbuzz(n)
-    return 'fizzbuzz' if n % 3 == 0 && n % 5 == 0
-    return 'fizz' if n % 3 == 0
-    return 'buzz' if n % 5 == 0
-    return "#{n}"
+  def fizzbuzz(num)
+    return 'fizzbuzz' if (num % 3).zero? && (num % 5).zero?
+    return 'fizz' if (num % 3).zero?
+    return 'buzz' if (num % 5).zero?
+
+    n.to_s
   end
 end
--- specs/solver_spec.rb
+++ specs/solver_spec.rb
@@ -12,11 +12,11 @@ describe Solver do
     end
     it 'should return 362,880 for the factorial of 9' do
       result = @solver.factorial(9)
-      expect(result).to eq 362880
+      expect(result).to eq 362_880
     end
     it 'should return 3,628,800 for the factorial of 10' do
       result = @solver.factorial(10)
-      expect(result).to eq 3628800
+      expect(result).to eq 3_628_800
     end
   end
 
@@ -29,25 +29,25 @@ describe Solver do
   end
 
   context 'When testing the fizzbuzz() method' do
-    it 'should return "fizz" when the parameter "n" is divisible by 3' do
+    it 'should return "fizz" when the parameter "num" is divisible by 3' do
       result = @solver.fizzbuzz(3)
       expect(result).to eq 'fizz'
     end
 
-    it 'should return "buzz" when the parameter "n" is divisible by 5' do
+    it 'should return "buzz" when the parameter "num" is divisible by 5' do
       result = @solver.fizzbuzz(5)
       expect(result).to eq 'buzz'
     end
 
-    it 'should return "fizzbuzz" when the parameter "n" is divisible by 3 AND 5' do
+    it 'should return "fizzbuzz" when the parameter "num" is divisible by 3 AND 5' do
       result = @solver.fizzbuzz(15)
       expect(result).to eq 'fizzbuzz'
     end
 
-    it 'should return the parameter "n" as a string when "n" is NOT divisible by 3 NOR 5' do
+    it 'should return the parameter "num" as a string when "num" is NOT divisible by 3 NOR 5' do
       n = 17
       result = @solver.fizzbuzz(n)
-      expect(result).to eq "#{n}"
+      expect(result).to eq n.to_s
     end
   end
 end
